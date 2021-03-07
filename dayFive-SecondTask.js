2a2c4a5b1b51d1d34aff050ea51158c089bfeb2d
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-07 18:54:33 +0530

    created a new file

---
+++ dayFive-SecondTask.js
@@ -0,0 +1,3 @@
+//Explain the following questions in your own words
+
+//What is the difference between forEach, map, filter and reduce ? Explain these using your own words Explain the difference between function declaration and arrow function ? Explain the difference between find and findIndex ? If you like to filter out one object element in an array which method do you like to use: filter or find ? Explain Explain the difference of var, let and const when we declare a variable ?
